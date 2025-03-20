b5eaa85a873ff020d10ef04caa058d046b6a34a1
Anthony-Phiri-533-MW <anthonyphiri533@gmail.com>
2025-03-20 09:55:53 +0200

    Add State managent  Added Zunstand to manage state in the app  #2  N/A  N/A

--- utils/zStore.ts
+++ utils/zStore.ts
@@ -1,8 +1,11 @@
 import { create } from 'zustand'
 
-const useStore = create((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-  updateBears: (newBears) => set({ bears: newBears }),
+type MeteredStore = {
+  petrol: number;
+  diseal: number;
+}
+
+export const useMeteredStore = create<MeteredStore>(() => ({
+  petrol:0,
+  diseal: 0
 }))
