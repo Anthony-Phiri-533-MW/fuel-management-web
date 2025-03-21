ebdab6e15b4c5dd2d2773d4e4d16a1f969a5f8f9
Anthony-Phiri-533-MW <anthonyphiri533@gmail.com>
2025-03-21 10:43:44 +0200

    LINT  Code linted
    6ef7ec2
     #1

--- app/components/Sidepanel/MeteredReadings/MeteredReadings.tsx
+++ app/components/Sidepanel/MeteredReadings/MeteredReadings.tsx
@@ -1,17 +1,14 @@
-import { useMeteredStore } from '@/utils/zStore';
+'use client'
+
 import React from 'react';
 import PumpForm from '../../Forms/PumpForm';
 
 const MeteredReadings: React.FC = () => {
-    const diseal = useMeteredStore((state) => state.diseal)
-    const petrol  = useMeteredStore((state) => state.petrol)
   return (
     <div>
       <h2 className="text-xl font-bold mb-4">Metered Readings</h2>
       <p>This is the Metered Readings page.</p>
       <PumpForm />
-      <h1 className='text-xl'>Petrol: {petrol}</h1>
-      <h1 className='text-xl'>diseal: {diseal}</h1>
     </div>
   );
 };
